922efbcea1ff931c01df4c252b421e9edc17cc12
Your Name <you@example.com>
2023-09-17 10:34:03 +0800

    update

--- rank/models/agg_prediction_info.py
+++ rank/models/agg_prediction_info.py
@@ -8,6 +8,7 @@ import numpy as np
 
 def agg():
     agg_result = dict()
+    agg_result["all_exp"] = dict()
     max_high_rate = -np.inf
     max_high_rate_config = None
     max_sharp_rate = -np.inf
@@ -16,7 +17,6 @@ def agg():
     max_avg_ap_config = None
     for label in tqdm(os.listdir(EXP_DIR)):
         if not label.startswith("y") and not label.startswith("dy"): continue
-        agg_result[label] = dict()
         label_dir = os.path.join(EXP_DIR, label)
         for exp_config in  os.listdir(label_dir):
             configured_exp_dir = os.path.join(label_dir, exp_config)
@@ -26,7 +26,7 @@ def agg():
             finished = True
             for val_start_day in os.listdir(configured_exp_dir):
                 val_start_day_dir = os.path.join(configured_exp_dir, val_start_day)
-                print(val_start_day_dir)
+                # print(val_start_day_dir)
                 
                 if not os.path.isdir(val_start_day_dir): continue
                 if not os.path.exists(os.path.join(val_start_day_dir, "meta.json")):
@@ -54,13 +54,14 @@ def agg():
             avg_low = np.mean(top3_low_means)
             avg_sharp = np.mean(top3_sharps)
             
-            agg_result[label][exp_config] = dict()
-            agg_result[label][exp_config]["avg_epoch"] = avg_epoch
-            agg_result[label][exp_config]["avg_ap"] = avg_ap
-            agg_result[label][exp_config]["avg_auc"] = avg_auc
-            agg_result[label][exp_config]["agv_high"] = agv_high
-            agg_result[label][exp_config]["avg_low"] = avg_low
-            agg_result[label][exp_config]["avg_sharp"] = avg_sharp
+            agg_result["all_exp"][exp_config] = dict()
+            agg_result["all_exp"][exp_config]["label"] = label
+            agg_result["all_exp"][exp_config]["avg_epoch"] = avg_epoch
+            agg_result["all_exp"][exp_config]["avg_ap"] = avg_ap
+            agg_result["all_exp"][exp_config]["avg_auc"] = avg_auc
+            agg_result["all_exp"][exp_config]["agv_high"] = agv_high
+            agg_result["all_exp"][exp_config]["avg_low"] = avg_low
+            agg_result["all_exp"][exp_config]["avg_sharp"] = avg_sharp
             
             if agv_high > max_high_rate:
                 max_high_rate = agv_high
@@ -98,6 +99,8 @@ def agg():
                     "avg_low": avg_low,
                     "avg_sharp": avg_sharp
                 }
+    agg_result["all_exp"] = sorted(agg_result["all_exp"].items(), key=lambda x:x[1]["avg_sharp"])
+    
     agg_result["best"] = dict()
     agg_result["best"]["ap"] = max_avg_ap_config
     agg_result["best"]["sharp"] = max_sharp_rate_config
