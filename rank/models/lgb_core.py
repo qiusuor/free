332f9a0c0989552ce115aab6f17e947f906a3067
Your Name <you@example.com>
2023-09-29 01:22:03 +0800

    update

--- rank/models/lgb_core.py
+++ rank/models/lgb_core.py
@@ -95,7 +95,7 @@ def train_lightgbm(argv):
             if "code_name" not in df.columns or not isinstance(df.code_name[-1], str) or "ST" in df.code_name[-1] or "st" in df.code_name[-1] or "sT" in df.code_name[-1]:
                 continue
             df["date"] = df.index
-            df = df[to_date(20220901):]
+            df = df.iloc[-250:]
             dataset.append(df)
         dataset = pd.concat(dataset, axis=0)
 
