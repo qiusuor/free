955f99422cd85db80951ca1c952f8d075377eaba
Your Name <you@example.com>
2023-10-04 22:04:11 +0800

    update

---
+++ data/inject_minute_feature.py
@@ -0,0 +1,50 @@
+import numpy as np
+import pandas as pd
+from talib import abstract
+import talib
+from multiprocessing import Pool
+from config import *
+from utils import *
+from tqdm import tqdm
+from joblib import dump
+import warnings
+from embedding.auto_encoder import MLPAutoEncoder
+import platform
+
+warnings.filterwarnings("ignore")
+
+
+def inject_one(path):
+    code = os.path.basename(path).split("_")[0]
+    data = joblib.load(path)
+    minutes_feat_path = os.path.join(MINUTE_FEAT, "{}_5_2.pkl".format(code))
+    minutes_feat = joblib.load(minutes_feat_path)
+    data = data.join(minutes_feat)
+    data.to_csv(path.replace(".pkl", ".csv"))
+    dump(data, path)
+    
+    
+    
+def inject_minute_feature():
+    paths = []
+    with torch.no_grad():
+        for file in tqdm(os.listdir(DAILY_DIR)):
+            code = file.split("_")[0]
+            if not_concern(code) or is_index(code):
+                continue
+            if not file.endswith(".pkl"):
+                continue
+            path = os.path.join(DAILY_DIR, file)
+            paths.append(path)
+    # print(paths[0])
+    # inject_one(paths[0])
+    pool = Pool(32)
+    pool.imap_unordered(inject_one, paths)
+    pool.close()
+    pool.join()
+            
+     
+if __name__ == "__main__":
+    inject_minute_feature()
+    
+    
