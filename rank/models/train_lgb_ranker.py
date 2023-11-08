87e73b42baf9e39a5a1010c563776b23d669db3a
Your Name <you@example.com>
2023-11-08 20:14:17 +0800

    update

---
+++ rank/models/train_lgb_ranker.py
@@ -0,0 +1,116 @@
+import joblib
+import lightgbm as lgb
+import pandas as pd
+from sklearn.metrics import mean_squared_error, roc_curve, auc, average_precision_score, roc_auc_score
+import numpy as np
+from tqdm import tqdm
+from multiprocessing import Pool
+from joblib import load, dump
+import _pickle as cPickle
+from multiprocessing import Process
+from utils import *
+from data.fetch_daily import fetch_daily
+from data.inject_features import inject_features
+from data.inject_labels import inject_labels
+from matplotlib import pyplot as plt
+import shutil
+import json
+from rank.models.lgb_core import *
+from rank.models.agg_prediction_info import agg_prediction_info
+import bisect
+import platform
+
+
+if __name__ == "__main__":
+    
+    df = joblib.load(os.path.join(DAILY_DIR, "sh.600000_d_2.pkl"))
+    if "code_name" not in df.columns:
+        prepare_data(update=False)
+    
+    search_labels = [
+        # "y_next_1d_close_2d_open_rate_rank_10%",
+        # "y_next_1d_close_2d_open_rate_rank_20%",
+        # "y_next_1d_close_2d_open_rate_rank_30%",
+        # "y_next_1d_close_2d_open_rate_rank_50%",
+        
+        # "y_2_d_close_high_rank_10%",
+        # "y_2_d_close_high_rank_20%",
+        # "y_2_d_close_high_rank_30%",
+        # "y_2_d_close_high_rank_50%",
+        
+        # "y_02_103",
+        # "y_02_105",
+        # "y_02_107",
+        "y_02_109",
+        # "y_next_1d_up_to_limit",
+        
+        # "y_2_d_high_rank_10%_safe_1d",
+        # "y_2_d_high_rank_20%_safe_1d",
+        # "y_2_d_high_rank_30%_safe_1d",
+        # "y_2_d_high_rank_50%_safe_1d",
+        
+        # "y_2_d_high_rank_10%",
+        # "y_2_d_high_rank_20%",
+        # "y_2_d_high_rank_30%",
+        # "y_2_d_high_rank_50%",
+        
+        # "y_2_d_ret_rank_10%",
+        # "y_2_d_ret_rank_20%",
+        # "y_2_d_ret_rank_30%",
+        # "y_2_d_ret_rank_50%",
+        
+        # "y_2_d_ret_rank_10%_safe_1d",
+        # "y_2_d_ret_rank_20%_safe_1d",
+        # "y_2_d_ret_rank_30%_safe_1d",
+        # "y_2_d_ret_rank_50%_safe_1d",
+    
+    ]
+    
+    features = get_feature_cols()
+    trade_days = get_trade_days(update=False)
+    trunc_index = bisect.bisect_right(trade_days, SEARCH_END_DAY)
+    trade_days = trade_days[:trunc_index]
+    cache_data = EXP_DATA_CACHE.format(trade_days[-1])
+    val_n_day = VAL_N_LAST_DAY
+    argvs = []
+    
+    
+    for label in search_labels:
+        for num_leaves in [3, 7, 15, 31, 63]:
+            for min_data_in_leaf in [11, 21, 41, 81]:
+                for max_depth in [3, 7, 9, 12]:
+                    if 2**max_depth <= num_leaves: continue
+                    for train_len in [250]:
+                    # for train_len in [30, 50, 120, 180]:
+                        n_day = get_n_val_day(label)
+                        
+                        print(len(argvs))
+                        # print(trade_days[-val_n_day-2*n_day:-2*n_day])
+                        
+                        for train_val_split_day in trade_days[-val_n_day-2*n_day-val_delay_day:-2*n_day-val_delay_day]:
+                            train_start_day = to_date(get_offset_trade_day(train_val_split_day,
+                                                                        -train_len))
+                            train_end_day = to_date(get_offset_trade_day(train_val_split_day, 0))
+                            
+                            val_start_day = to_date(get_offset_trade_day(train_val_split_day, 1 + val_delay_day))
+                            val_end_day = to_date(get_offset_trade_day(to_int_date(val_start_day), n_day-1))
+                            argv = [
+                                features, label, train_start_day, train_end_day, val_start_day,
+                                val_end_day, n_day, train_len, num_leaves, max_depth, min_data_in_leaf, cache_data, -1
+                            ]
+                            if not os.path.exists(cache_data):
+                                # print(argv)
+                                train_lightgbm(argv)
+                                print("Generate cache file this time, try again.")
+                                exit(0)
+                            argvs.append(argv)
+                        # exit(0)
+                        
+
+    np.random.shuffle(argvs)
+    pool = Pool(32 if "Linux" in platform.platform() else 2)
+    pool.imap_unordered(train_lightgbm, argvs)
+    pool.close()
+    pool.join()
+    agg_prediction_info(last_n_day=val_n_day)
+    
