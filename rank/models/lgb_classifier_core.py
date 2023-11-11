14ebae364a873cd1bacc57c6daa158ddf498e442
Your Name <you@example.com>
2023-11-11 14:11:35 +0800

    update

--- rank/models/lgb_classifier_core.py
+++ rank/models/lgb_classifier_core.py
@@ -34,7 +34,7 @@ def topk_shot(data, label, k=10, watch_list=[]):
         watches[watch+f"_topk_{k}_max"] = data[watch][:k].max()
         watches[watch+f"_topk_{k}_min"] = data[watch][:k].min()
         watches[watch+f"_topk_{k}_mean"] = data[watch][:k].mean()
-    watches[f"sharp_{k}"] = watches[f"y_next_2_d_high_ratio_topk_{k}_mean"] * watches[f"y_next_2_d_high_ratio_topk_{k}_mean"]
+    watches[f"sharp_{k}"] = watches[f"y_next_2_d_high_ratio_topk_{k}_mean"] * watches[f"y_next_2_d_low_ratio_topk_{k}_mean"]
     return miss_cnt, shot_cnt, watches
 
 def train_val_data_filter(df):
