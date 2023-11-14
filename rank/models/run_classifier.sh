4b65a95ecdc226c8b3237bc444c304b2a70fd8e1
Your Name <you@example.com>
2023-11-15 01:51:19 +0800

    update

--- rank/models/run_classifier.sh
+++ rank/models/run_classifier.sh
@@ -1,5 +1,5 @@
 set -x -e
-export SEARCH_END_DAY=20230603
+export SEARCH_END_DAY=22990401
 python /home/qiusuo/free/data/discard.py
 python rank/models/train_lgb_classifier.py
 python rank/models/train_lgb_classifier.py
--- rank/models/run_ranker.sh
+++ rank/models/run_ranker.sh
@@ -1,4 +1,6 @@
 set -x -e
+export SEARCH_END_DAY=22990401
+python /home/qiusuo/free/data/discard.py
 python rank/models/train_lgb_ranker.py
 python rank/models/train_lgb_ranker.py
 python rank/models/serve_lgb_ranker.py
