1e5c968f483f0c2995600d64e47adbacadc6a3f7
Your Name <you@example.com>
2023-11-15 20:52:39 +0800

    update

--- rank/models/run_classifier.sh
+++ rank/models/run_classifier.sh
@@ -1,5 +1,5 @@
 set -x -e
-export SEARCH_END_DAY=22990401
+export SEARCH_END_DAY=21990101
 python /home/qiusuo/free/data/discard.py
 python rank/models/train_lgb_classifier.py
 python rank/models/train_lgb_classifier.py
