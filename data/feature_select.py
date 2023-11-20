740f0302817cc0d6c0068ffacb2d2c081f98955b
Your Name <you@example.com>
2023-11-20 19:57:26 +0800

    update

--- data/feature_select.py
+++ data/feature_select.py
@@ -1,4 +1,4 @@
-from scipy.stats import entropy
+from scipy.stats import entropy, wasserstein_distance
 from utils import *
 from config import *
 import bisect
@@ -9,7 +9,7 @@ import numpy as np
 def get_quantile(series):
     return series.quantile(np.linspace(0.0, 1.0, 10)).values
 
-def calc_bin_distribution_kl(series_all, series_train, series_val):
+def calc_bin_distribution_dist(series_all, series_train, series_val, method="wsd"):
     bins = get_quantile(series_all)
     def map_to_bin(x):
         index = bisect.bisect_left(bins, x)
@@ -18,12 +18,22 @@ def calc_bin_distribution_kl(series_all, series_train, series_val):
         c = Counter(series)
         return [c[i] for i in range(len(bins))]
     
-    series_train = to_sorted_distribution(series_train.apply(map_to_bin))
-    series_val = to_sorted_distribution(series_val.apply(map_to_bin))
-    kl = entropy(series_val, series_train)
-    if np.isnan(kl):
-        kl = np.inf
-    return kl
+    series_train = series_train.apply(map_to_bin)
+    series_val = series_val.apply(map_to_bin)
+
+    if method == "wsd":
+        return wasserstein_distance(series_val, series_train)
+    elif method == "kl":
+        series_train = to_sorted_distribution(series_train)
+        series_val = to_sorted_distribution(series_val)
+        kl = entropy(series_val, series_train)
+        if np.isnan(kl):
+            kl = np.inf
+        return kl
+    else:
+        raise NotImplementedError
+
+
 
 def get_rel_dist(a, b):
     return abs(a - b) / max(min(a, b), 1e-9)
@@ -42,11 +52,6 @@ def analyse_single_feature(train_start_day, train_end_day, val_start_day, val_en
         dataset.append(df)
         groups.append(len(df))
         dates.append(int(file[:-4]))
-        
-    # train_start_day = to_int_date(train_start_day)
-    # train_end_day = to_int_date(train_end_day)
-    # val_start_day = to_int_date(val_start_day)
-    # val_end_day = to_int_date(val_end_day)
     
     train_dataset, val_dataset = [], []
     train_groups, val_groups = [], []
@@ -57,26 +62,30 @@ def analyse_single_feature(train_start_day, train_end_day, val_start_day, val_en
         elif val_start_day <= date <= val_end_day:
             val_dataset.append(data_day)
             val_groups.append(g)
+    all_dateset = pd.concat(dataset)
     train_dataset = pd.concat(train_dataset)
     val_dataset = pd.concat(val_dataset)
     train_label = train_dataset[label]
     val_label = val_dataset[label]
+    all_data_label = all_dateset[label]
     
     feature_alignment = []
     for feature in get_feature_cols():
-        # all_data_feature = all_dateset[feature][all_data_label].astype(float)
+        all_data_feature = all_dateset[feature][all_data_label].astype(float)
         train_feature = train_dataset[feature][train_label].astype(float)
         val_feature = val_dataset[feature][val_label].astype(float)
         skew_dis = get_abs_dist(train_feature.skew(), val_feature.skew())
         kurt_dis = get_abs_dist(train_feature.kurt(), val_feature.kurt())
         mean_dis = get_abs_dist(train_feature.mean(), val_feature.mean())
-        # kl =  calc_bin_distribution_kl(all_data_feature, train_feature, val_feature)
-        feature_alignment.append([feature, mean_dis, skew_dis, kurt_dis])
+        prob_dis =  calc_bin_distribution_dist(all_data_feature, train_feature, val_feature)
+        feature_alignment.append([feature, prob_dis, mean_dis, skew_dis, kurt_dis])
+        print(feature, prob_dis, mean_dis, skew_dis, kurt_dis)
+        
         # print(feature, kl)
-    feature_alignment = pd.DataFrame(feature_alignment, columns=["features", "mean_value_dist", "skew_dist", "kurt_dist"])
+    feature_alignment = pd.DataFrame(feature_alignment, columns=["features", "prob_dis", "mean_value_dist", "skew_dist", "kurt_dist"])
     feature_alignment = feature_alignment.sort_values(by="mean_value_dist", ascending=False)
     feature_alignment.to_csv("feature_kl.csv", index=False)
-    print(list(feature_alignment[feature_alignment.mean_value_dist > 0.10].features))
+    print(list(feature_alignment[feature_alignment.prob_dis > 0.10].features))
         
 if __name__ == "__main__":
     analyse_single_feature(20220824, 20230904, 20231025, 20231115, "y_02_109")
