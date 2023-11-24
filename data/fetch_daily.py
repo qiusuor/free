e1978e5df23583ee5b67a18ca80f1506cb00bd69
yuqiang <yuqiang@HUAWEI-MateBook-14-2023.local>
2023-11-24 17:51:10 +0800

    update

--- data/fetch_daily.py
+++ data/fetch_daily.py
@@ -1,11 +1,13 @@
 from data.fetch_core import fetch
 from utils import *
+import platform
 
 def fetch_daily():
     return fetch(freqs=['d'], num_thread=8)
 
 if __name__ == "__main__":
     code_num = fetch_daily()
-    if not code_num: exit(0)
-    upload_data()
+    if not code_num: exit(-1)
+    if "Windows" in platform.platform():
+        upload_data()
     
