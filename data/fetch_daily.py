8abb09d1390e1e88fb454c57653110ce3dc6f3c1
qiusuo <22>
2023-12-09 16:12:06 +0800

    update

--- data/fetch_daily.py
+++ data/fetch_daily.py
@@ -3,7 +3,6 @@ from utils import *
 import platform
 from data.check_data import check_daily
 from data.discard import discard_labels
-from data.inject_labels import inject_labels
 
 def fetch_daily():
     return fetch(freqs=['d'], num_thread=8)
@@ -13,7 +12,6 @@ if __name__ == "__main__":
     if not code_num: exit(-1)
     discard_labels()
     check_daily()
-    inject_labels()
     if "Windows" in platform.platform():
         upload_data()
     
