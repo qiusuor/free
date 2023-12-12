83906962168a840e0fa2688ad2c44fa61c268519
Your Name <you@example.com>
2023-12-12 21:13:11 +0800

    update

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ THREAD_NUM = cpu_count()
 SEARCH_END_DAY = 21990101
 if "SEARCH_END_DAY" in os.environ:
     SEARCH_END_DAY = int(os.environ["SEARCH_END_DAY"])
-VAL_N_LAST_DAY = 2
-TEST_N_LAST_DAY = 2
+VAL_N_LAST_DAY = 5
+TEST_N_LAST_DAY = 5
 val_delay_day = 30
 
 ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
