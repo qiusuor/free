503914423f632b89359047371538ff4fdf2dbe49
yuqiang <yuqiang@192.168.0.188>
2024-01-13 20:57:56 +0800

    update

--- data/inject_features.py
+++ data/inject_features.py
@@ -211,18 +211,7 @@ def inject_style_feature(df):
     
 def inject_one(path):
     df = joblib.load(path)
-    
-    # inject_ta_features(df)
-    # inject_chip_features(df)
-    # inject_price_turn_features(df)
-    # inject_alpha_features(df)
     inject_style_feature(df)
-    
-    # minu_feat_path = os.path.join(MINUTE_FEAT, os.path.basename(path).replace("_d_2", "_1_3"))
-    # minu_feat = joblib.load(minu_feat_path)
-    # minu_feat = minu_feat.set_index("date")
-    # df = df.join(minu_feat, how="left")
-
     df.to_csv(path.replace(".pkl", ".csv"))
     dump(df, path)
     
