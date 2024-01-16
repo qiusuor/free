256514ac72c22d2d1022ecf0bb3739a9b62f6a13
Your Name <you@example.com>
2024-01-16 19:42:10 +0800

    update

--- rank/models/run_classifier.sh
+++ rank/models/run_classifier.sh
@@ -2,7 +2,7 @@ set -x -e
 export SEARCH_END_DAY=21990101
 LOG=run_classifer.log
 rm -rf $LOG
-python /home/qiusuo/free/data/discard.py >>$LOG
+python data/discard.py >>$LOG
 python rank/models/train_lgb_classifier.py >>$LOG
 python rank/models/train_lgb_classifier.py >>$LOG
 python rank/models/serve_lgb_classifier.py >>$LOG
