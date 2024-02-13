e8289d7cea49f56fd9f88d6bc7b1cc83e31705f9
qiusuo <22>
2024-02-13 22:59:47 +0800

    update

--- data/plot_style_feedback.py
+++ data/plot_style_feedback.py
@@ -20,6 +20,13 @@ groups = {
     # "high_price": [col for col in df.columns if "high_price" in col],
     # "high_turn": [col for col in df.columns if "high_turn" in col],
 }
-# for name, group in groups.items():
-#     profile = ProfileReport(df[group], title="Profiling Report")
-#     profile.to_file("style_feedback_{}.html".format(name))
+for name, group in groups.items():
+    profile = ProfileReport(df[group], title="Profiling Report")
+    profile.to_file("style_feedback_{}.html".format(name))
+
+df = df.iloc[-120:]
+for filed in fileds:
+    df["y_next_1d_ret_mean_{}".format(filed)+"_cum"] = (1 + df["style_feat_shif1_of_y_next_1d_ret_mean_"+filed]).cumprod()
+    
+keys = [col for col in df.columns if "_cum" in col]
+render_html(df[keys], filed, "cum_retr.html")
