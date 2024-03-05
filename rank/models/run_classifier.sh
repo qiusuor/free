8757ace52c636e0fba94447ae062555df3336b07
Your Name <you@example.com>
2024-03-05 18:15:42 +0800

    update

--- rank/models/run_classifier.sh
+++ rank/models/run_classifier.sh
@@ -3,6 +3,7 @@ export SEARCH_END_DAY=21990101
 LOG=run_classifer.log
 rm -rf $LOG
 python data/discard.py >>$LOG
+python data/update_tdx_minutes.py
 python data/prepare_minutes_features.py >>$LOG
 python rank/models/train_lgb_classifier.py >>$LOG
 python rank/models/train_lgb_classifier.py >>$LOG
